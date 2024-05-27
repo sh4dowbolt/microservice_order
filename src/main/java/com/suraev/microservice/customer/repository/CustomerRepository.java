c1f37f93b78d035d6d3788dd55bab87757e7e06c
Sh4dowbolt <suraevvvitaly@gmail.com>
2024-05-27 18:36:30 +0400

    some cleaning project

--- src/main/java/com/suraev/microservice/customer/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.suraev.microservice.customer.repository;
-
-import com.suraev.microservice.customer.domain.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
